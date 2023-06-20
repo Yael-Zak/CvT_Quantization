d45bc743f785f1e47d55823f530689743b9835fb
Matan Millionshik <mmatan@campus.technion.ac.il>
2023-06-20 20:02:43 +0300

    updated quantiztion file to start loading model

--- Quantization.py
+++ Quantization.py
@@ -1,16 +1,18 @@
 # for now this script only deals with how to load the base model with all of it's weights for validation
-
+import torch
 from transformers import CvtConfig, CvtModel, CvtForImageClassification
 
 # Initializing a Cvt msft/cvt style configuration
 configuration = CvtConfig()
 
 # Initializing a model (with random weights) from the msft/cvt style configuration
-model1 = CvtModel(configuration)
-
+model = CvtModel(configuration)
+base_model_state_dict = model.state_dict()
+new_state_dict = {}
+for index, layer_name in enumerate(base_model_state_dict.keys()):
+    # print("layer name is:" + layer_name + "\nlayer tesnor is of size:" + str(layer_tensor.size()))
+    new_state_dict[layer_name] = base_model_state_dict[layer_name].half()
 # Accessing the model configuration
-configuration = model1.config
-
-model2 = CvtForImageClassification.from_pretrained("microsoft/cvt-13", output_hidden_states=True, return_dict=True)
+model.load_state_dict(new_state_dict)
 
 print("Finished! stop here ahsheli")
